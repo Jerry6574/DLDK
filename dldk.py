73a660f549fc7c3e23464e3d6d773df00dcc1a8e
jerryw <wu@assmann-wsw.com>
2019-08-22 15:52:34 -0700

    added get_n_items

--- dldk.py
+++ dldk.py
@@ -20,7 +20,26 @@ def get_webdriver(chrome_options=None):
 class DLDK:
     def __init__(self, url):
         self.url = url
-        self.n_item = 0
 
     def get_n_items(self):
-        soup = bs4.BeautifulSoup()
+        browser = get_webdriver()
+        browser.get(self.url)
+
+        num_re = re.compile(r"[^0-9]")
+        n_items_str = browser.find_element_by_id("matching-records-count").text
+        n_items = int(re.sub(num_re, "", n_items_str))
+
+        browser.close()
+        browser.quit()
+
+        return n_items
+
+
+def main():
+    url = "https://www.digikey.com/products/en/connectors-interconnects/terminal-blocks-headers-plugs-and-sockets/370"
+    dldk = DLDK(url)
+    print(dldk.get_n_items())
+
+
+if __name__ == '__main__':
+    main()
