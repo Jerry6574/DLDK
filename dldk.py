df25da421fcd612d3e02a070af33422b4c91d38a
jerryw <wu@assmann-wsw.com>
2019-08-22 15:56:31 -0700

    added get_n_pages

--- dldk.py
+++ dldk.py
@@ -20,6 +20,8 @@ def get_webdriver(chrome_options=None):
 class DLDK:
     def __init__(self, url):
         self.url = url
+        self.n_items = self.get_n_items()
+        self.n_pages = self.get_n_pages()
 
     def get_n_items(self):
         browser = get_webdriver()
@@ -34,11 +36,23 @@ class DLDK:
 
         return n_items
 
+    def get_n_pages(self):
+        page_size = 500
+
+        if self.n_items <= 500:
+            n_pages = 1
+        elif self.n_items % page_size == 0:
+            self.n_items = self.n_items // page_size
+        else:
+            n_pages = self.n_items // page_size + 1
+
+        return n_pages
+
 
 def main():
     url = "https://www.digikey.com/products/en/connectors-interconnects/terminal-blocks-headers-plugs-and-sockets/370"
     dldk = DLDK(url)
-    print(dldk.get_n_items())
+    print(dldk.n_items, dldk.n_pages)
 
 
 if __name__ == '__main__':
